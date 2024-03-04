fd98420f433de928440caf98cc7ad52b4ffb33b7
Micheal <82182216+SageMain@users.noreply.github.com>
2024-03-04 13:51:11 -0700

    Adding Firebase implementation
    Setup basic Firebase implementation, I just followed the same steps we did in Lab 5 to create one through the website, and attach it to our project

--- HolosProject/app/src/main/java/com/example/holosproject/MainActivity.java
+++ HolosProject/app/src/main/java/com/example/holosproject/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring a private variable to hold a reference to the Firestore database
+    private FirebaseFirestore database;
+    // This next line is an example of what it would look like to declare a CollectionReference Variable,
+    // In Firestore, a collection is a group of documents. A CollectionReference is a reference to a specific collection in the Firestore database.
+    private CollectionReference userAccountNamesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing the Firestore database instance when the activity is created
+        database = FirebaseFirestore.getInstance();
+        // This next line initializes "userAccountNamesRef" by obtaining a reference to the "Profile Account Names" collection in the Firestore database.
+        // userAccountNamesRef holds a reference to the "Proifle Account Names" collection in our database.
+        userAccountNamesRef = database.collection("Profile Account Names");
     }
 }
