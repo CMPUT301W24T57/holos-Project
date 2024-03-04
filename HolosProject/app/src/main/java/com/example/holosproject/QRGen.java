3323022558bc0172d49881d248259986e216bbb5
Nicolas Woo <nlhwoo03@gmail.com>
2024-03-04 16:52:25 -0700

    added the ability to save the qr code as a file
    what works:
    scanning
    generating QR code from plaintext
    saving QR code to the phone

--- HolosProject/app/src/main/java/com/example/holosproject/QRGen.java
+++ HolosProject/app/src/main/java/com/example/holosproject/QRGen.java
@@ -1,26 +1,40 @@
 package com.example.holosproject;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.File;
+import java.io.OutputStream;
+import java.util.Objects;
+
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
+import androidmads.library.qrgenearator.QRGSaver;
 
 public class QRGen extends AppCompatActivity {
 // see https://github.com/androidmads/QRGenerator
-private String savePath = Environment.getExternalStorageDirectory().getPath() + "/QRCode/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +62,26 @@ private String savePath = Environment.getExternalStorageDirectory().getPath() + 
                     e.printStackTrace();
                 }
             }
+        });
+
+        findViewById(R.id.save_btn).setOnClickListener(v -> {
+                OutputStream fos;
+                try {
+                    QRGEncoder qrgEncoder = new QRGEncoder(editText.getText().toString(), null, QRGContents.Type.TEXT, 200);
+                    Bitmap bitmap = qrgEncoder.getBitmap(0);
+                    ContentResolver resolver = getContentResolver();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, editText.getText().toString() + "Code" + ".jpg");
+                    contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
+                    Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                    fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    Objects.requireNonNull(fos);
+                    Toast.makeText(QRGen.this, "Image saved", Toast.LENGTH_LONG).show();
+                    editText.setText(null);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
         });
     }
 }
