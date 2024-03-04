cfe6d9359c5916b8f5abc85e8696cd1de9a08173
Micheal <82182216+SageMain@users.noreply.github.com>
2024-03-04 14:56:51 -0700

    Basic First Time Open Screen
    working on what appears when a user first opens the app, spending little to no time designing the ui just focusing on getting stuff implemented

---
+++ HolosProject/app/src/main/java/com/example/holosproject/IntroActivity.java
@@ -0,0 +1,33 @@
+package com.example.holosproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity { // AppCombatActivity provides various methods and features to manage the lifecycle of an activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Setting content view to be the activity_intro.xml file
+        setContentView(R.layout.activity_intro);
+
+        Button buttonCreateProfile = findViewById(R.id.AttendeeButton);
+
+        // Creating OnClickListener for the AttendeeButton (What happens when we press "Event Attendee")
+        buttonCreateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creating an intent to change from activity_intro to FirstTimeProfileCreationActivity
+                Intent intent = new Intent(IntroActivity.this, FirstTimeProfileCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
