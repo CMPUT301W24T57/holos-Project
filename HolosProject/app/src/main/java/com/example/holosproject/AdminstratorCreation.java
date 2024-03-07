ef8fb45abd46bf8bbd13165166d532c99eea3436
Micheal <82182216+SageMain@users.noreply.github.com>
2024-03-07 15:33:36 -0700

    testing

--- HolosProject/app/src/main/java/com/example/holosproject/AdminstratorCreation.java
+++ HolosProject/app/src/main/java/com/example/holosproject/AdminstratorCreation.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+// testing test testing
+
 public class AdminstratorCreation extends AppCompatActivity {
 
     @Override
