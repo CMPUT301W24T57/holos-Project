5ccedc9858b48994a811d0e6c5271b57ba14fdac
dreicouv <querubin@ualberta.ca>
2024-03-07 09:30:55 -0700

    Finished creating the dashboard

--- HolosProject/app/src/main/java/com/example/holosproject/OrganizerDashboardEventsAdapter.java
+++ HolosProject/app/src/main/java/com/example/holosproject/OrganizerDashboardEventsAdapter.java
@@ -1,4 +1,51 @@
 package com.example.holosproject;
 
-public class OrganizerDashboardEventsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OrganizerDashboardEventsAdapter extends RecyclerView.Adapter<OrganizerDashboardEventsAdapter.EventViewHolder>  {
+    private List<Event> eventList;
+
+    public OrganizerDashboardEventsAdapter(List<Event> eventList) {
+
+        this.eventList = eventList;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.organizer_dashboard, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EventViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.eventTitleTextView.setText(event.getName());
+        holder.eventDateTimeTextView.setText(event.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return eventList.size();
+    }
+
+    static class EventViewHolder extends RecyclerView.ViewHolder {
+        TextView eventTitleTextView;
+        TextView eventDateTimeTextView;
+
+        EventViewHolder(View itemView) {
+            super(itemView);
+            eventTitleTextView = itemView.findViewById(R.id.textViewEventName);
+            eventDateTimeTextView = itemView.findViewById(R.id.textViewEventDate);
+        }
+    }
 }
